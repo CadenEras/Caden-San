8924fadb26cca693d38a2ba0d2c3ef76e6ee4c3c
CadenEras <gries.melissa@outlook.fr>
2024-08-15 09:45:22 +0200

    :sparkle: Added deploy-commands.js

---
+++ src/deply-commands.js
@@ -0,0 +1,57 @@
+/**@format */
+
+const { REST, Routes } = require("discord.js");
+const config = require("./Config/config.json");
+const fs = require("fs");
+const path = require("path");
+
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(__dirname, "../Commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ("data" in command && "execute" in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(
+				`${currentDate} => [WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+			streamKonsole.log(
+				`${currentDate} => [WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(config.token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			//for guild level / debugging
+			Routes.applicationGuildCommands(config.selfId, config.baseGuildId),
+			//for global level
+			// Routes.applicationCommands(clientId),
+			{ body: commands }
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.log(`${currentDate} => An error occured while deploying commands: ${error}`);
+		streamKonsole.log(`${currentDate} => An error occured while deploying commands: ${error}`);
+	}
+})();
